f575a6cd45bc122ad5b34a58916587c109d7f2b1
mielcorpuz <jomiel.banog2x@gmail.com>
2025-05-16 04:17:24 +0800

    follow up

--- database/migrations/2025_05_11_134233_create_employees_table.php
+++ database/migrations/2025_05_11_134233_create_employees_table.php
@@ -52,10 +52,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('employees', function (Blueprint $table) {
-        $table->dropForeign(['team_id']);
-        $table->dropForeign(['user_id']);
-    });
         Schema::dropIfExists('employees');
     }
 };
